e04cd0c16edf47efb2d99a4fdbc97e1fb6ba15f6
Laurence <snowsnooks@gmail.com>
2019-01-05 16:31:07 +0000

    slideshow

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 window.onload = function() {
   init();
+  showSlides();
 }
 function init(){
   console.log('init fired');
 } 
-//var button = document.getElementById('muteButton');
 function toggleMute(){
   var vid = document.getElementById("headerVideo");
   var but = document.getElementById("muteButton")
@@ -20,4 +20,17 @@ function toggleMute(){
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+var slideIndex = 0;
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 9000); // Change image every 2 seconds
 }
